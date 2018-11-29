cece49c6c4a8ec41615009c68bac9fec80a0ea26
Jose Hernandez Torres <jose@hernandezt.com>
2018-11-28 20:51:08 -0700

    Updated html and CSS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 var search = $("#exampleFormControlInput1").val();
 var beginDate = $("#begin").val();
 
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&begin_date=" + beginDate + "&apikey=e16a8f856f654bdd81bccef373d3f5ef"
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&apikey=e16a8f856f654bdd81bccef373d3f5ef";
 
 $("#search").on("click", function(event){
 
@@ -9,9 +9,9 @@ $("#search").on("click", function(event){
 
 $.ajax({
   url: queryUrl,
-  method: 'GET',
+  method: 'GET'
 }).then(function (response){
-    console.log(response)
+    console.log(response);
 
 })
 })
